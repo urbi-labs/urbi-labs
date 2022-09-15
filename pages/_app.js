3d0d5d7e92bdd8dd3adcf78cfdaadc944e86abfc
Nando Delgado <nando.emmanuel@gmail.com>
2022-09-14 23:19:13 -0300

    Base theme with styled components wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components";
+import Layout from '../components/Layout'
+import theme from "../styles/theme";
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider theme={theme}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ThemeProvider>
+	)
 }
 
 export default MyApp
---
+++ styles/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+	colors: {
+		blue: "#015DFE",
+		lightestGray: "#E5E5E5"
+	}
+}
+
+export default theme
